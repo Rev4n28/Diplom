f6d229a9c999963536da16a2d362c2e4830d2881
Rev4n28 <pavlik.kolev@mail.ru>
2025-09-23 20:03:32 +0500

    change main

--- bank/banks/bank/admin.py
+++ bank/banks/bank/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Password, Profile
+
+admin.site.register(Password)
+admin.site.register(Profile)
--- bank/banks/bank/models.py
+++ bank/banks/bank/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True)
+    name = models.CharField(max_length=200, blank=True)
+    email = models.EmailField(max_length=500, blank=True)
+    username = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.username
 
 
 class Password(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True, default="123456")
     title = models.CharField(max_length=200)
     password = models.TextField(max_length=200)
 
